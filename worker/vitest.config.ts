4f50a2c78f3a4c4924e25e1539f34d63f2a31100
Tri Tran <thuutri2710@gmail.com>
2026-06-10 11:55:20 +0700

    Test trigger build worker

--- worker/vitest.config.ts
+++ worker/vitest.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    include: ["test/**/*.test.ts"],
     environment: "node",
+    include: ["test/**/*.test.ts"],
   },
 });
